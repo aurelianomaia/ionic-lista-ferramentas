c2e3c4b0e9af3488c79c0d2a86dbb26aeaeec3ce
aurelianomaia <e.scola@hotmail.com>
2019-11-16 10:19:52 -0300

    adicionar arquivo total

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import{NavController}from '@ionic/angular';
+import {Storage} from'@ionic/storage';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  inputtext: string;
+  key: string = 'username';
+
+  constructor(public navCtrl: NavController,
+    private storage: Storage) {
+
+  }
+  saveData(){
+    this.storage.set(this.key,this.inputtext)
+  }
+
+  loadData(){
+    this.storage.get(this.key).then((val) =>{
+      console.log('your age is' ,val);
+    });
+  }
+}
